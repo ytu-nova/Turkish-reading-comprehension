f1abf5a3508207358616f961817789071227182b
Goker Cebeci <goker@cebeci.name>
2019-06-01 19:11:25 +0300

    sentence based approach

--- scikit-sentences-ngram.py
+++ scikit-sentences-ngram.py
@@ -16,10 +16,10 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 def sentence_based_unsimilar(d):
     # her secenege en cok benzeyen cumlenin indexini bulur
-    a = [ max(d[:,j]) for j in range(d.shape[1]) ]
+    a = [max(d[:, j]) for j in range(d.shape[1])]
 
     # her secenegin kendisine en yakin cumleye olan benzerligi
-    v = [ a[j] for j in range(d.shape[1]) ]
+    v = [a[j] for j in range(d.shape[1])]
 
     return np.argmin(v)
 
@@ -88,25 +88,26 @@ def kn(data, labels, test, isSimilarity):
 def ngram_str(s, N=3):
     s = s.replace(" ", "_")
     ngram = NGram(N=N)
-    return " ".join(ngram.ngrams(s))
+    return " ".join(ngram.split(s))
 
 
 def sanitize(t):
     s = re.sub(r'[^a-zğüşıöç\s+]|\n|\t|\r', '', t.lower(), flags=re.IGNORECASE)
-    return ngram_str(s)
+    return s#ngram_str(s)
 
 
 def process_data(q, doSaitize):
     labels = ['A', 'B', 'C', 'D', 'E']
     texts = [q['answers']['A'], q['answers']['B'], q['answers']['C'], q['answers']['D'], q['answers']['E']]
-    texts = map(lambda x: sanitize(x), texts) if doSaitize else texts
-    tfidf = TfidfVectorizer()  # CountVectorizer()
-    data = tfidf.fit_transform(texts)
-
+    #texts = map(lambda x: sanitize(x), texts) if doSaitize else texts
+    #vectorizer = TfidfVectorizer()  # CountVectorizer()
+    #vectorizer = TfidfVectorizer(preprocessor=sanitize, ngram_range=(1, 2), analyzer="word")
+    vectorizer = TfidfVectorizer(preprocessor=sanitize, ngram_range=(3, 4), analyzer="char")
+    data = vectorizer.fit_transform(texts)
+    #print(vectorizer.get_feature_names())
     test = q['paragraph'].replace('?', '.').replace('!', '.').split('.')
-    test = map(lambda x: sanitize(x), test) if doSaitize else test
-
-    test = tfidf.transform(test)
+    #test = map(lambda x: sanitize(x), test) if doSaitize else test
+    test = vectorizer.transform(test)
     return data, labels, test
 
 
@@ -183,17 +184,23 @@ with open('data.json') as json_file:
 
         results.append(result)
 
-
         if simCount > 0 and unsimCount > 0:
             count = int(q['no'])
             log = {'no': q['no']
-                , 'nb': corrects['nb'] / count, 'nb-sim': sim['nb'] / simCount, 'nb-unsim': unsim['nb'] / unsimCount
+                , 'nb': [corrects['nb'] / count, corrects['nb']], 'nb-sim': sim['nb'] / simCount, 'nb-unsim': unsim['nb'] / unsimCount
                 , 'sgd': corrects['sgd'] / count, 'sgd-sim': sim['sgd'] / simCount,
                    'sgd-unsim': unsim['sgd'] / unsimCount
                 , 'rf': corrects['rf'] / count, 'rf-sim': sim['rf'] / simCount, 'rf-unsim': unsim['rf'] / unsimCount
                 , 'lr': corrects['lr'] / count, 'lr-sim': sim['lr'] / simCount, 'lr-unsim': unsim['lr'] / unsimCount
                 , 'kn': corrects['kn'] / count, 'kn-sim': sim['kn'] / simCount, 'kn-unsim': unsim['kn'] / unsimCount}
+            log = {
+                'nb': [corrects['nb'] / count, corrects['nb'], count],
+                'nb-sim': [sim['nb'] / simCount, sim['nb'], simCount],
+                'nb-unsim': [unsim['nb'] / unsimCount, unsim['nb'], unsimCount]
+            }
             print(count, end='\r')
+
+        #break;
 
     with open('results-sentences-ngram.json', 'w') as outfile:
         json.dump(results, outfile)
--- scikit.py
+++ scikit.py
@@ -1,5 +1,6 @@
 import json
 import re
+from ngram import NGram
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
@@ -57,20 +58,27 @@ def kn(data, labels, test, isSimilarity):
     return predicted[0][0] if isSimilarity else predicted[4][0]
 
 
+def ngram_str(s, N=3):
+    s = s.replace(" ", "_")
+    ngram = NGram(N=N)
+    return " ".join(ngram.split(s))
+
+
 def sanitize(t):
-    return re.sub(r'[^a-zğüşıöç\s+]|\n|\t|\r', '', t.lower(), flags=re.IGNORECASE)
+    s = re.sub(r'[^a-zğüşıöç\s+]|\n|\t|\r', '', t, flags=re.IGNORECASE)
+    return s  # ngram_str(s)
 
 
 def process_data(q, doSaitize):
     labels = ['A', 'B', 'C', 'D', 'E']
     texts = [q['answers']['A'], q['answers']['B'], q['answers']['C'], q['answers']['D'], q['answers']['E']]
-    texts = map(lambda x: sanitize(x), texts) if doSaitize else texts
-    tfidf = TfidfVectorizer()  # CountVectorizer()
-    # tfidf = CountVectorizer()
-    data = tfidf.fit_transform(texts)
-    test = sanitize(q['paragraph']) if doSaitize else q['paragraph']
-    test = tfidf.transform([test])
+    #vectorizer = TfidfVectorizer(preprocessor=sanitize, ngram_range=(1, 2), analyzer="word")
+    vectorizer = TfidfVectorizer(preprocessor=sanitize, ngram_range=(3, 4), analyzer="char")
+    data = vectorizer.fit_transform(texts)
+    # print(vectorizer.get_feature_names())
+    test = vectorizer.transform([q['paragraph']])
     return data, labels, test
+
 
 results = []
 corrects = {'nb': 0, 'sgd': 0, 'rf': 0, 'lr': 0, 'kn': 0}
@@ -78,8 +86,6 @@ sim = {'nb': 0, 'sgd': 0, 'rf': 0, 'lr': 0, 'kn': 0}
 unsim = {'nb': 0, 'sgd': 0, 'rf': 0, 'lr': 0, 'kn': 0}
 simCount = 0
 unsimCount = 0
-
-
 
 with open('data.json') as json_file:
     data = json.load(json_file)
@@ -149,10 +155,17 @@ with open('data.json') as json_file:
             count = int(q['no'])
             log = {'no': q['no']
                 , 'nb': corrects['nb'] / count, 'nb-sim': sim['nb'] / simCount, 'nb-unsim': unsim['nb'] / unsimCount
-                , 'sgd': corrects['sgd'] / count, 'sgd-sim': sim['sgd'] / simCount, 'sgd-unsim': unsim['sgd'] / unsimCount
+                , 'sgd': corrects['sgd'] / count, 'sgd-sim': sim['sgd'] / simCount,
+                   'sgd-unsim': unsim['sgd'] / unsimCount
                 , 'rf': corrects['rf'] / count, 'rf-sim': sim['rf'] / simCount, 'rf-unsim': unsim['rf'] / unsimCount
                 , 'lr': corrects['lr'] / count, 'lr-sim': sim['lr'] / simCount, 'lr-unsim': unsim['lr'] / unsimCount
                 , 'kn': corrects['kn'] / count, 'kn-sim': sim['kn'] / simCount, 'kn-unsim': unsim['kn'] / unsimCount}
+
+            log = {
+                'nb': [corrects['nb'] / count, corrects['nb'], count],
+                'nb-sim': [sim['nb'] / simCount, sim['nb'], simCount],
+                'nb-unsim': [unsim['nb'] / unsimCount, unsim['nb'], unsimCount]
+            }
             print(count, end='\r')
 
     with open('results.json', 'w') as outfile:
